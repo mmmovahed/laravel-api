b6deb9bcd8ba9dbd4a5dbc3f38e3dfb5a8f615bb
Mohammad Mahdi <work.mohamad.mahdi@gmail.com>
2025-05-16 16:42:43 +0330

    Changing the routes with new rule.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\api\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
