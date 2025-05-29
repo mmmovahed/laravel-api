473a5cb4469c8d4bbd8f1dbb547eee483b267eae
Mohammad Mahdi <work.mohamad.mahdi@gmail.com>
2025-05-29 20:56:56 +0330

    Course requests

---
+++ app/Http/Requests/api/v1/updateCourseRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\api\v1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class updateCourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()?->isAdmin();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category_id' => 'sometimes|exists:categories,id',
+            'college_id' => 'sometimes|exists:colleges,id',
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'teacher' => 'sometimes|string|max:255',
+            'status' => 'sometimes|in:active,inactive',
+        ];
+    }
+}
