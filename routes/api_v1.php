56119acef6ccb1054b578a90cf08064ad5045fe7
Mohammad Mahdi <work.mohamad.mahdi@gmail.com>
2025-06-04 12:34:50 +0330

    HTTP syntacs in Route files

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\V1\courseController;
-use App\Http\Controllers\AuthController;
+use App\HTTP\Controllers\Api\V1\courseController;
+use App\HTTP\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
