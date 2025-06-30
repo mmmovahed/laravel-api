5e29d80a04ac3c9ebe2270e47bbc8ffb881cd908
Mohammad Mahdi <work.mohamad.mahdi@gmail.com>
2025-06-30 20:25:15 +0330

    Changing the route for Auth.

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,10 @@
 use App\Http\Controllers\api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('v1/register', [AuthController::class, 'register']);
+Route::post('v1/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('v1/logout', [AuthController::class, 'logout']);
+    Route::get('v1/user', [AuthController::class, 'user']);
 });
